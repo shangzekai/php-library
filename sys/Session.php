123c241bf51e56bf682d0a29a9f60b264fe4d300
shangzekai <shangzeki@163.com>
2018-03-09 17:17:05 +0800

    Update Session.php

--- sys/Session.php
+++ sys/Session.php
@@ -1,121 +1,259 @@
 <?php
 /**
- * Custom Session Handle Process
+ * 业务监控系统用户会话
  *
- * Comply Access From The Database
+ * 管理用户会话和相关工具
  *
- * @author      Chiak<chiaki.sun@yeahmobi.com>
- * @package     library/sys
- * @since       Version 1.0 @2014-05-29
+ * @author      chiak<Chiak@yeahmobi.com>
+ * @package     library/user
+ * @since       Version 1.0 @2014-05-28
+ * @copyright   Copyright (c) 2014, YeahMobi, Inc.
  */
-class Sys_Session {
+class User_Session {
 
-    private $dao;
-    private $table;
-    private $lifeTime;
-    private static $instance;
-
-    public function __construct()
-    {
-        // setting session handler callback function
-        session_set_save_handler(
-            array($this, 'open'),
-            array($this, 'close'),
-            array($this, 'read'),
-            array($this, 'write'),
-            array($this, 'destroy'),
-            array($this, 'gc')
-        );
-        // get session max lifetime in config
-        $this->lifeTime = ini_get('session.gc_maxlifetime');
-        // get database dao object
-        $this->dao = new Ym_Dao('log');
-
-        $this->table  = Sys_Database::getTable('system_session');
-    }
+    // 用户登陆密码的混淆字符，除非系统废除等特殊原因否则常量值永远不变
+    const LOGIN_SALT = 'cf45eef37f17184eca18fda007067fa8';
+    // 用户登录会话的混淆字符
+    const SESSION_SALT = '96d9645d58abe1e7ce29987c6c1453d4';
+    // 必须登陆
+    const LOGIN_MUST = '1';
+    // 无需登录
+    const LOGIN_WITHOUT = '2';
 
     /**
-     * Single Mode, Return This Object
-     * @return Sys_Session
+     * 自定义可逆加密
+     * @param $data 加密数据
+     * @return string
      */
-    public static function getInstance()
+    public function encrypt($data)
     {
-        if(self::$instance == null) {
-            self::$instance = new self();
+        $key	=	self::SESSION_SALT;
+        $x		=	0;
+        $len	=	strlen($data);
+        $l		=	strlen($key);
+        $char = '';
+        $str = '';
+        for ($i = 0; $i < $len; $i++)
+        {
+            if ($x == $l)
+            {
+                $x = 0;
+            }
+            $char .= $key{$x};
+            $x++;
         }
-        return self::$instance;
+        for ($i = 0; $i < $len; $i++)
+        {
+            $str .= chr(ord($data{$i}) + (ord($char{$i})) % 256);
+        }
+        return base64_encode($str);
     }
 
     /**
-     * Open Session Interface
-     * @param $savePath
-     * @param $sessionName
+     * 自定义可逆解密
+     * @param $data 解密数据
+     * @return string
+     */
+    public function decrypt($data)
+    {
+        $key = self::SESSION_SALT;
+        $x = 0;
+        $data = base64_decode($data);
+        $len = strlen($data);
+        $l = strlen($key);
+        $char = '';
+        $str = '';
+        for ($i = 0; $i < $len; $i++)
+        {
+            if ($x == $l)
+            {
+                $x = 0;
+            }
+            $char .= substr($key, $x, 1);
+            $x++;
+        }
+        for ($i = 0; $i < $len; $i++)
+        {
+            if (ord(substr($data, $i, 1)) < ord(substr($char, $i, 1)))
+            {
+                $str .= chr((ord(substr($data, $i, 1)) + 256) - ord(substr($char, $i, 1)));
+            }
+            else
+            {
+                $str .= chr(ord(substr($data, $i, 1)) - ord(substr($char, $i, 1)));
+            }
+        }
+        return $str;
+    }
+
+    /**
+     * 检测登陆状态
      * @return bool
      */
-    public function open($savePath, $sessionName)
+    public static function detectLogin()
     {
-        if(!$this->dao) {
+        $loginCookie = self::getLoginCookie();
+        $loginSession = self::getLoginSession();
+
+        if(empty($loginCookie) || $loginCookie != $loginSession) {
+            // 登陆异常，删除登陆Cookie，更新登陆会话状态为异常
+            self::delLoginCookie();
+            self::delLoginSession();
             return false;
         }
         return true;
     }
 
     /**
-     * Close Session Interface
-     * @return bool
+     * 设置登录Cookie
+     * @param $loginInfo
      */
-    public function close()
+    public static function setLoginCookie($loginInfo)
     {
-        return true;
+        // 加密用户信息字符串
+        $info = self::encrypt(serialize($loginInfo));
+        setcookie("LOGIN_INFO", $info, time() + 3600 * 24, '/');
     }
 
     /**
-     * Read Session
-     * @param $id
+     * 获取登陆Cookie
      * @return mixed
      */
-    public function read($id)
-    {
-        $sql = sprintf("SELECT `value` FROM `%s` WHERE `id` = '%s'", $this->table, $id);
-        $session = $this->dao->queryRow($sql, true);
-        return $session['value'];
+    public static function getLoginCookie(){
+        $loginInfo = null;
+        if(isset($_COOKIE['LOGIN_INFO'])) {
+            $loginInfo = unserialize(self::decrypt($_COOKIE['LOGIN_INFO']));
+        }
+        return $loginInfo;
     }
 
     /**
-     * Write Session
-     * @param $id
-     * @param $value
-     * @return bool
+     * 删除登陆Cookie
      */
-    public function write($id, $value)
+    public static function delLoginCookie()
     {
-        $expire = time() + $this->lifeTime;
-        $sql = sprintf("REPLACE INTO `%s` VALUES('%s', '%s', '%d')", $this->table, $id, $value, $expire);
-        return $this->dao->query($sql);
+        setcookie('LOGIN_INFO', '', time() - 3600, '/');
     }
 
     /**
-     * Destroy Session
-     * @param $id
-     * @return bool
+     * 设置登录IDCookie
+     * @param $uid
      */
-    public function destroy($id)
-    {
-        $where = sprintf("`id ` = '%s'", $id);
-        return $this->dao->delete($this->table, $where);
+    public static function setUserIdCookie($uid) {
+        // 加密用户信息字符串
+        setcookie("USER_ID", $uid, time() + 3600 * 24, '/');
     }
 
     /**
-     * Destroy Expired Session
-     * @param $maxLifeTime
-     * @return bool
+     * 获取登陆Cookie
+     * @return mixed
      */
-    public function gc($maxLifeTime)
-    {
-        $nowTime = time();
-        $where = sprintf("`expire` < '%s'", $nowTime);
-        return $this->dao->delete($this->table, $where);
+    public static function getUserIdCookie(){
+        $returns = null;
+        if(isset($_COOKIE['USER_ID'])) {
+            $returns = $_COOKIE['USER_ID'];
+        }
+        return $returns;
     }
 
+    /**
+     * 设置登录IDCookie
+     * @param $business
+     */
+    public static function setBusiness($business) {
+        // 加密用户信息字符串
+        setcookie("USER_BUSINESS", $business, time() + 3600 * 24, '/');
+    }
+
+    /**
+     * 获取登陆Cookie
+     * @return mixed
+     */
+    public static function getBusiness(){
+        $returns = null;
+        if(isset($_COOKIE['USER_BUSINESS'])) {
+            $returns = $_COOKIE['USER_BUSINESS'];
+        }
+        return $returns;
+    }
+
+    /**
+     * 删除登陆Cookie
+     */
+    public static function delUserIdCookie()
+    {
+        setcookie('USER_ID', '', time() - 3600, '/');
+    }
+
+    /**
+     * 设置登录会话
+     * @param $loginInfo
+     */
+    public static function setLoginSession($loginInfo) {
+        $loginInfo = serialize($loginInfo);
+        Yaf_Session::getInstance()->set('LOGIN_INFO', $loginInfo);
+    }
+
+    /**
+     * 获取登陆会话
+     * @return mixed
+     */
+    public static function getLoginSession(){
+        $loginSession = unserialize(Yaf_Session::getInstance()->get('LOGIN_INFO'));
+        return $loginSession;
+    }
+
+    /**
+     * 删除登陆会话
+     */
+    public static function delLoginSession() {
+        Yaf_Session::getInstance()->del('LOGIN_INFO');
+    }
+
+    /**
+     * 设置返回页面
+     * @param $backPage
+     */
+    public static function setBackPage($backPage)
+    {
+        Yaf_Session::getInstance()->set('BACK_PAGE', $backPage);
+    }
+
+    /**
+     * 获取返回页面
+     * @return mixed
+     */
+    public static function getBackPage()
+    {
+        return Yaf_Session::getInstance()->get('BACK_PAGE');
+    }
+
+    /**
+     * 设置SESSION通知消息
+     * @param $notice
+     */
+    public static function setNotice($notice)
+    {
+        $notice = serialize($notice);
+        Yaf_Session::getInstance()->set('NOTICE', $notice);
+    }
+
+    /**
+     * 从SESSION获取通知消息
+     * @return mixed
+     */
+    public static function getNotice()
+    {
+        $notice = unserialize(Yaf_Session::getInstance()->get('NOTICE'));
+        return $notice;
+    }
+
+    /**
+     * 从SESSION删除通知消息
+     */
+    public static function delNotice()
+    {
+        Yaf_Session::getInstance()->del('NOTICE');
+    }
 
 }
